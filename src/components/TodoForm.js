03c55243db1c26cc04c8cf6cbbcd1307448760e8
Joris Bertier <joris.bertier@gmail.com>
2024-03-05 09:37:54 -0500

    form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const TodoForm = () => {
   return (
-    <div>TodoForm</div>
+    <form className='TodoForm'>
+        <input type='text' clasName='todo-input' placeholder='Quel est la tâche du jour'></input>
+        <button type='submit' className='todo-btn'></button>
+    </form>
   )
 }
