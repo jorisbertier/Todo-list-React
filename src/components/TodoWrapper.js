de0fde2296665ab3842c92e058ea98a6b98547e6
Joris Bertier <joris.bertier@gmail.com>
2024-03-05 11:16:47 -0500

    add component todo & create

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
+import { Todo } from './Todo'
 import {v4 as uuidv4} from 'uuid'
 uuidv4()
 
@@ -16,6 +17,7 @@ export const TodoWrapper = () => {
   return (
     <div className="TodoWrapper">
         <TodoForm addTodo={addTodo}/>
+        <Todo />
     </div>
   )
 }
